4e2de27992766a151a1ecd1acd01b8cfe8eb0625
Lynn Garren <garren@fnal.gov>
2021-10-27 12:37:30 -0500

    find genie is almost working

--- Modules/FindGENIE.cmake
+++ Modules/FindGENIE.cmake
@@ -1,54 +1,67 @@
 #[================================================================[.rst:
 FindGENIE
 ----------
-  find GENIE and all its libraries
+
+Finds GENIE and all its components
+
+
 
 #]================================================================]
-if (NOT GENIE_FOUND)
-  if (GENIE_FIND_REQUIRED)
-    set(_cet_GENIE_FIND_REQUIRED ${GENIE_FIND_REQUIRED})
-    unset(GENIE_FIND_REQUIRED)
-  else()
-    unset(_cet_GENIE_FIND_REQUIRED)
-  endif()
-  find_package(GENIE CONFIG QUIET)
-  if (_cet_GENIE_FIND_REQUIRED)
-    set(GENIE_FIND_REQUIRED ${_cet_GENIE_FIND_REQUIRED})
-    unset(_cet_GENIE_FIND_REQUIRED)
-  endif()
-  if (GENIE_FOUND)
-    set(_cet_GENIE_config_mode CONFIG_MODE)
-  else()
-    unset(_cet_GENIE_config_mode)
+# headers
+find_file(_cet_Messenger_h NAMES Messenger.h HINTS ENV GENIE_INC
+  PATH_SUFFIXES GENIE/Framework/Messenger)
+if (_cet_Messenger_h)
+  get_filename_component(_cet_GENIE_include_dir "${_cet_Messenger_h}" PATH)
+  get_filename_component(_cet_GENIE_include_dir "${_cet_GENIE_include_dir}" PATH)
+  get_filename_component(_cet_GENIE_include_dir "${_cet_GENIE_include_dir}" PATH)
+  get_filename_component(_cet_GENIE_include_dir "${_cet_GENIE_include_dir}" PATH)
+  if (_cet_GENIE_include_dir STREQUAL "/")
+    unset(_cet_GENIE_include_dir)
   endif()
 endif()
-if (NOT GENIE_FOUND)
-  find_file(_cet_GENIE_h NAMES Messenger.h HINTS ENV GENIE_INC
-    PATH_SUFFIXES GENIE/Framework/Messenger)
-  if (_cet_GENIE_h)
-    get_filename_component(_cet_GENIE_include_dir "${_cet_GENIE_h}" PATH)
-    get_filename_component(_cet_GENIE_include_dir "${_cet_GENIE_include_dir}" PATH)
-    get_filename_component(_cet_GENIE_include_dir "${_cet_GENIE_include_dir}" PATH)
-    if (_cet_GENIE_include_dir STREQUAL "/")
-      unset(_cet_GENIE_include_dir)
+if (EXISTS "${_cet_GENIE_include_dir}")
+  set(GENIE_FOUND TRUE)
+  get_filename_component(_cet_GENIE_dir "${_cet_GENIE_include_dir}" PATH)
+  if (_cet_GENIE_dir STREQUAL "/")
+    unset(_cet_GENIE_dir)
+  endif()
+  set(GENIE_INCLUDE_DIRS "${_cet_GENIE_include_dir}")
+  set(GENIE_LIBRARY_DIR "${_cet_GENIE_dir}/lib")
+endif()
+if (GENIE_FOUND)
+  set(GENIE_LIB_LIST)
+  set(_cet_genie_libs GFwMsg GFwReg GFwAlg GFwInt GFwGHEP GFwNum GFwUtl GFwParDat
+                  GFwEG GFwNtp GPhXSIg GPhPDF GPhNuclSt GPhCmn GPhDcy GPhHadTransp
+                  GPhHadnz GPhDeEx GPhAMNGXS GPhAMNGEG GPhChmXS GPhCohXS GPhCohEG
+                  GPhDISXS GPhDISEG GPhDfrcXS GPhDfrcEG GPhGlwResXS GPhGlwResEG
+                  GPhIBDXS GPhIBDEG GPhMNucXS GPhMNucEG GPhMEL GPhNuElXS GPhNuElEG
+                  GPhQELXS GPhQELEG GPhResXS GPhResEG GPhStrXS
+                  GPhStrEG GPhNDcy GTlGeo GTlFlx GRwFwk GRwIO GRwClc)
+  foreach (_glib IN LISTS _cet_genie_libs)
+    find_library(${_glib}_LIBRARY NAMES ${_glib} PATHS ${GENIE_LIBRARY_DIR})
+    if(${_glib}_LIBRARY)
+      if (NOT TARGET GENIE::${_glib})
+        add_library(GENIE::${_glib} SHARED IMPORTED)
+        set_target_properties(GENIE::${_glib} PROPERTIES
+          INTERFACE_INCLUDE_DIRECTORIES "${GENIE_INCLUDE_DIRS}"
+          IMPORTED_LOCATION "${${_glib}_LIBRARY}"
+          )
+      endif()
+      list(APPEND GENIE_LIB_LIST GENIE::${_glib})
     endif()
-  endif()
-  if (EXISTS "${_cet_GENIE_include_dir}")
-    set(GENIE_FOUND TRUE)
-  endif()
-endif()
-if (GENIE_FOUND AND _cet_GENIE_include_dir AND NOT TARGET GFwMsg::GFwMsg)
-  add_library(GFwMsg::GFwMsg INTERFACE IMPORTED)
-  set_target_properties(GFwMsg::GFwMsg PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${_cet_GENIE_include_dir}")
+  endforeach()
 endif()
 
-set(GENIE_FIND_REQUIRED ${_cet_GENIE_FIND_REQUIRED})
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GENIE ${_cet_GENIE_config_mode}
-  REQUIRED_VARS GENIE_FOUND)
+find_package_handle_standard_args(GENIE
+  REQUIRED_VARS GENIE_FOUND
+  GENIE_INCLUDE_DIRS
+  GENIE_LIB_LIST)
 
 unset(_cet_GENIE_FIND_REQUIRED)
-unset(_cet_GENIE_config_mode)
-unset(_cet_GENIE_h CACHE)
+unset(_cet_GENIE_dir)
+unset(_cet_GENIE_include_dir)
+unset(_glib)
+unset(_cet_genie_libs)
+unset(_cet_Messenger_h CACHE)
 
